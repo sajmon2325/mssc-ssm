90e004fb75600863ee7c5762eea927894e29652b
Simon <sajmon2325@gmail.com>
2020-08-11 10:37:00 +0200

    State Machine Enumerations

---
+++ src/test/java/com/opensourcedev/msscssm/MsscSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensourcedev.msscssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
